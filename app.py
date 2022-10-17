2bc099b36419ff9a6bd0accd7a25826cb2e18ae6
Santiago Ariza <santiariza15@hotmail.com>
2022-10-18 04:02:24 +1000

    All done (:

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import contextlib
 import streamlit as st
 from io import StringIO
 import random
 import pandas as pd
 import requests
 import json
+from itertools import combinations
 
-file = st.file_uploader("Please choose a file")
+st.title('Yu-Gi-Oh! Brick Calculator')
+
+file = st.file_uploader("Please choose a file",type="ydk")
 
 if file is not None:
     stringio = StringIO(file.getvalue().decode("utf-8"))
@@ -18,30 +20,33 @@ if file is not None:
         if "extra" in card or "side" in card:
             break
     first = st.checkbox('Going first: ', True)
-    if first:
-        cards_in_hand = 5
-    else:
-        cards_in_hand = 6
+    cards_in_hand = 5 if first else 6
     df = pd.DataFrame (main, columns=["Cards"])
     main_string = ",".join(df["Cards"])
     URL = f'https://db.ygoprodeck.com/api/v7/cardinfo.php?id={main_string}'
     page = requests.get(URL).json()
     df_deck = pd.read_json(json.dumps(page["data"]))
-    one_card_combos = st.multiselect(
-    'Select your one card combo cards:',
-    df_deck["name"]
-    )
-    two_card_combos = []
     drawing_cards = st.multiselect(
     'Select your drawing cards:',
-    df_deck[~df_deck.name.isin(one_card_combos)]["name"],
+    df_deck["name"],
+    help = "Select the cards that make you draw. This reduces the size of the deck in calculations."
     )
-    for card in drawing_cards:
-        with contextlib.suppress(ValueError):
-            while True:
-                main.remove(card)
+    deck_no_drawing_cards = df_deck[~df_deck.name.isin(drawing_cards)]["name"].to_list()
+    one_card_combos = st.multiselect(
+    'Select your one card combo cards:',
+    deck_no_drawing_cards,
+    help = "Select the cards that single handedly jump start your engine."
+    )
+    list_removed = drawing_cards + one_card_combos
+    deck_names_no_one_combos = df_deck[~df_deck.name.isin(list_removed)]["name"].to_list()
+    two_card_combos = st.multiselect(
+    'Select the pair of two card combos:',
+    list(combinations(deck_names_no_one_combos, 2)),
+    help = "Select the pair of cards needed for your engine to start."
+    )
+    
     flag = False
-    opening_hands = [random.choices(df_deck["name"],k=cards_in_hand) for _ in range(5000)]
+    opening_hands = [random.choices(deck_no_drawing_cards,k=cards_in_hand) for _ in range(5000)]
     counter = 0
     for hand in opening_hands:
         for cards in one_card_combos:
