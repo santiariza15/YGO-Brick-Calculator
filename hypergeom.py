e959d7d4eec3e8b965083532c0abec5696b44c3e
Santiago Ariza <santiariza15@hotmail.com>
2022-10-21 18:33:21 +1000

    Update hypergeom.py

--- hypergeom.py
+++ hypergeom.py
@@ -1,12 +1,14 @@
 from scipy.stats import hypergeom
 
+with open('Floowandereeze.ydk', 'r') as f:
+  deck = f.read().splitlines()
+
 k = 1
-M = 40
-n = 3 + 3 + 3
+M = len(deck) - deck.count('49238328') - deck.count('98645731')
+n = deck.count('18940725') + deck.count('28126717') + deck.count('69087397')
 N = 5
 hpd = hypergeom(M, n, N)
 prob = 1 -hypergeom.cdf(k-1, M, n, N)
-
 print(prob)
 
 
